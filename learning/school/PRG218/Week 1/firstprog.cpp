8478dd87c2929dafda4aa6cd763d2a6d01fb38b7
Eric <nightshadedude@gmail.com>
2018-05-02 03:53:44 +0000

    saved and commited - oops

---
+++ learning/school/PRG218/Week 1/firstprog.cpp
@@ -0,0 +1,17 @@
+// This is the first program that just writes out a simple message
+/*
+Eric Landeis
+PRG218
+firstprog.cpp
+*/
+
+// needed to perform C++ I/O using namespace std; 
+#include <iostream>
+
+int main ()
+{
+    cout << "Now is the time for all good men" << endl;
+    cout << "To come to the aid of their party" << endl;
+
+    return 0;
+}
