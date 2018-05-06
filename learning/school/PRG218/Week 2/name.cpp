3868071d209fe740f7fbf74a668a6f0293c2d8b7
Eric <nightshadedude@gmail.com>
2018-05-06 16:14:53 +0000

    name with blank info

---
+++ learning/school/PRG218/Week 2/name.cpp
@@ -0,0 +1,18 @@
+// This program will write the name, address and telephone
+// number of the programmer.
+// PLACE YOUR NAME HERE
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string firstName = "First";
+    string lastName = "Last";
+    string address = "123 Main Street";
+    string 
+// Fill in this space to write your first and last name
+// Fill in this space to write your address (on new line)
+// Fill in this space to write you city, state and zip (on new line)
+// Fill in this space to write your telephone number (on new line)
+return 0;
+}
