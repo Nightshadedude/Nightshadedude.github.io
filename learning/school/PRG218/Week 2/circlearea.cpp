54f94bc8be3d944b90964dacb41c7fd097590e4b
Eric <nightshadedude@gmail.com>
2018-05-11 03:56:12 +0000

    blanks filled in

--- learning/school/PRG218/Week 2/circlearea.cpp
+++ learning/school/PRG218/Week 2/circlearea.cpp
@@ -1,6 +1,12 @@
 // This program will output the circumference and area
 // of the circle with a given radius.
-// PLACE YOUR NAME HERE
+
+/*
+Eric Landeis
+PRG218
+circleapp.cpp
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -8,20 +14,21 @@ const double PI = 3.14;
 const double RADIUS = 5.4;
 
 int main(){
-    ________ area // definition of area of circle
+    float area; // definition of area of circle
     
     float circumference; // definition of circumference
     
     circumference = 2 * PI * RADIUS; // computes circumference
     
-    area = _____________ ; // computes area
+    area = PI * RADIUS * RADIUS; // computes area
     
     // Fill in the code for the cout statement that will output (with description)
     // the 
-    
+    cout << "Circumference = " << circumference << endl;
     
     // Fill in the code for the cout statement that will output (with description)
     // the area of the circle
+    cout << "Area = " << area << endl;
     
     return 0; 
 }
