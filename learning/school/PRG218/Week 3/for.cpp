e8a420761b40598fe48f18d0360f34de4c6c87af
Eric <nightshadedude@gmail.com>
2018-05-20 04:41:43 +0000

    exercise 3 completed

--- learning/school/PRG218/Week 3/for.cpp
+++ learning/school/PRG218/Week 3/for.cpp
@@ -37,22 +37,32 @@ mean of 3, 4, 5, 6, 7, 8, and 9, which is 6.
 using namespace std;
 int main()
 {
-    int value; // value is some positive number n
+    int startValue; // start value is some positive number n
+    int endValue; // end value is some positive number m
     int total = 0; // total holds the sum of the first n positive numbers
     int number; // the amount of numbers
     float mean; // the average of the first n positive numbers
-    cout << "Please enter a positive integer" << endl;
-    cin >> value;
-    if (value > 0)
+    cout << "Please enter a positive integer (starting value)" << endl;
+    cin >> startValue;
+    cout << "Please enter an ending value integer greater than or equal to the starting value" << endl;
+    cin >> endValue;
+
+    if ((startValue > 0) && (endValue > 0))
     {
-        for (number = 1; number <= value; number++)
+        if(startValue <= endValue)
         {
-            total = total + number;
-        } // curly braces are optional since there is only one statement
-        mean = static_cast<float>(total) / value; // note the use of the typecast
-        // operator here
-        cout << "The mean average of the first " << value
-        << " positive integers is " << mean << endl;
+            for (number = startValue; number <= endValue; number++)
+            {
+                total = total + number;
+            } // curly braces are optional since there is only one statement
+            mean = static_cast<float>(total) / (endValue - startValue + 1); // note the use of the typecast
+            // operator here
+            cout << "The mean average of the first " << (endValue - startValue + 1)
+            << " positive integers (from " << startValue << "-" << endValue
+            << ") is " << mean << endl;
+        }
+        else
+        cout << "Ending value  must be greater than or equal to the starting value" << endl;
     }
     else
     cout << "Invalid input - integer must be positive" << endl;
