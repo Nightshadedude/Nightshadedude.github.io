6efe5943c366cdd86ed255f0372c6e7317363e80
Eric <nightshadedude@gmail.com>
2018-05-20 04:47:30 +0000

    initial add

---
+++ learning/school/PRG218/Week 3/nested.cpp
@@ -0,0 +1,54 @@
+// This program finds the average time spent programming by a student
+// each day over a three day period.
+
+/*
+Eric Landeis
+PRG218
+for.cpp
+*/
+
+/*
+Exercise 1: Note that the inner loop of this program is always executed exactly
+three times—once for each day of the long weekend. Modify the code so
+that the inner loop iterates n times, where n is a positive integer input by
+the user. In other words, let the user decide how many days to consider
+just as they choose how many students to consider.
+
+
+Exercise 2: Modify the program from Exercise 1 so that it also finds the average
+number of hours per day that a given student studies biology as well as
+programming. For each given student include two prompts, one for each
+subject. Have the program print out which subject the student, on average,
+spent the most time on.
+
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int numStudents;
+float numHours, total, average;
+int student,day = 0; // these are the counters for the loops
+cout << "This program will find the average number of hours a day"
+<< " that a student spent programming over a long weekend\n\n";
+cout << "How many students are there ?" << endl << endl;
+cin >> numStudents;
+for(student = 1; student <= numStudents; student++)
+{
+total = 0;
+for(day = 1; day <= 3; day++)
+{
+cout << "Please enter the number of hours worked by student "
+<< student <<" on day " << day << "." << endl;
+cin >> numHours;
+total = total + numHours;
+}
+average = total / 3;
+cout << endl;
+cout << "The average number of hours per day spent programming by "
+<< "student " << student << " is " << average
+<< endl << endl << endl;
+}
+return 0;
+}
