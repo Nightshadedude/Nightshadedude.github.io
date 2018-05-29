39cf6b32e5c33646b6f6a647c243f47fd15e42ae
Eric <nightshadedude@gmail.com>
2018-05-29 01:45:53 +0000

    week 5 temp project

---
+++ learning/school/PRG218/Week 5/animal_communication.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+// Animal is a base class.
+class Animal
+{
+    public:
+    // Constructor
+    Animal()
+    { cout << "Animal constructor executing.\n"; }
+
+    // Destructor
+    virtual ~Animal()
+    {cout << "Animal destructor executing.\n"; }
+    
+    void communicate()
+    {
+        cout << "Speak" << endl;
+    }
+};
+
+// The Dog class is derived from Animal
+class Dog : public Animal
+{
+    public:
+        // Constructor
+        Dog() : Animal()
+        { cout << "Dog constructor executing.\n"; }
+
+        // Destructor
+        ~Dog()
+        { cout << "Dog destructor executing.\n"; }
+        
+        void communicate()
+        {
+            cout << "Woof" << endl;
+        }
+};
+
+// The Cat class is derived from Animal
+class Cat : public Animal
+{
+    public:
+        // Constructor
+        Cat() : Animal()
+        { cout << "Cat constructor executing.\n"; }
+
+        // Destructor
+        ~Cat()
+        { cout << "Cat destructor executing.\n"; }
+        
+        void communicate()
+        {
+            cout << "Meow" << endl;
+        }
+};
+
+//*************************************************
+// main function                                  *
+//*************************************************
+
+int main()
+{
+    Animal genericAnimal;
+    genericAnimal.communicate();
+
+    Dog ralph;
+    ralph.communicate();
+    
+    Cat fluffy;
+    fluffy.communicate();
+
+    return 0;
+}
