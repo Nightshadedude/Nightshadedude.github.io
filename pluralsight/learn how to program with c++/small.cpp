be5400282bc30e84b01ffa7681f8a2ce7ac43ba6
Eric <nightshadedude@gmail.com>
2018-09-11 18:26:30 +0000

    variable basics

--- pluralsight/learn how to program with c++/small.cpp
+++ pluralsight/learn how to program with c++/small.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello!" << endl;
-	cout << 3.1415 << endl;
+	cout << "Hello!" << endl << 2+2 << endl << endl;
+
+    int i;
+    
+    i = 3;
+    
+    cout << i << endl;
+
 	return 0;
 }
