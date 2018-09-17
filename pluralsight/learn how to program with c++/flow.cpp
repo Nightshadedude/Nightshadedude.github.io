1bcf99dfeadb075b64a2f039c1065e8d04da5ada
Eric <nightshadedude@gmail.com>
2018-09-17 18:31:03 +0000

    flow control example added else if

--- pluralsight/learn how to program with c++/flow.cpp
+++ pluralsight/learn how to program with c++/flow.cpp
@@ -5,20 +5,25 @@ int main()
 {
     int i;
     int j;
-    cout << "Enter a number";
+    cout << "Enter a number: ";
     cin >> i;
     cout << "You entered " << i << ".  Enter another number: ";
     cin >> j;
     
     if(i<j)
     {
-        cout << "The first number, " << i << "is less than the second number, "
-        << j << ".";
+        cout << "The first number, " << i << " is less than the second number, "
+        << j << "." << endl;
+    }
+    else if(i==j)
+    {
+        cout << "The first number, " << i << " is equal to the second number, "
+        << j << "." << endl;
     }
     else
     {
-        cout << "The first number, " << i << "is greather than or equal to the second number, "
-        << j << ".";
+        cout << "The first number, " << i << " is greater than the second number, "
+        << j << "." << endl;  
     }
 	return 0;
 }
