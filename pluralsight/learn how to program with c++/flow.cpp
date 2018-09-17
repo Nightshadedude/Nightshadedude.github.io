e047e850b2dffd96d093eaedb93ecbe043bbdf1c
Eric <nightshadedude@gmail.com>
2018-09-17 18:34:00 +0000

    flow control example added while

--- pluralsight/learn how to program with c++/flow.cpp
+++ pluralsight/learn how to program with c++/flow.cpp
@@ -5,25 +5,37 @@ int main()
 {
     int i;
     int j;
-    cout << "Enter a number: ";
-    cin >> i;
-    cout << "You entered " << i << ".  Enter another number: ";
-    cin >> j;
-    
-    if(i<j)
+    bool keepgoing = true;
+    int answer;
+    while(keepgoing)
     {
-        cout << "The first number, " << i << " is less than the second number, "
-        << j << "." << endl;
-    }
-    else if(i==j)
-    {
-        cout << "The first number, " << i << " is equal to the second number, "
-        << j << "." << endl;
-    }
-    else
-    {
-        cout << "The first number, " << i << " is greater than the second number, "
-        << j << "." << endl;  
+        cout << "Enter a number: ";
+        cin >> i;
+        cout << "You entered " << i << ".  Enter another number: ";
+        cin >> j;
+        
+        if(i<j)
+        {
+            cout << "The first number, " << i << " is less than the second number, "
+            << j << "." << endl;
+        }
+        else if(i==j)
+        {
+            cout << "The first number, " << i << " is equal to the second number, "
+            << j << "." << endl;
+        }
+        else
+        {
+            cout << "The first number, " << i << " is greater than the second number, "
+            << j << "." << endl;  
+        }
+        
+        cout << "Compare another? 0 for no: ";
+        cin >> answer;
+        if (answer == 0)
+        {
+            keepgoing = false;
+        }
     }
 	return 0;
 }
